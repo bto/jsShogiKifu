574447b76d89809433e6e428f9284030bb3ca049
Masato Bito <masato@bz2.jp>
2011-04-13 14:26:11 +0900

    added tests for parse info of Kifu.kif

--- test/test_kifu_kif.js
+++ test/test_kifu_kif.js
@@ -9,7 +9,7 @@ module('Kifu.Kif', {
   }
 });
 
-test('parse info', 0, function() {
+test('parse info', 24, function() {
   var kifu  = kifu_obj.kifu();
   var info  = kifu['info'];
   var info2 = Kifu().kifu()['info'];
@@ -19,6 +19,83 @@ test('parse info', 0, function() {
   var line = '対局ID：246';
   ok(Kifu.Kif.parseByLine(line, kifu), line);
   same(info, info2, line+' info');
+
+  // 開始日時
+  info2['start_time'] = new Date(2010, 9, 14, 9);
+  var line = '開始日時：2010/10/14 9:00';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 終了日時
+  info2['end_time'] = new Date(2010, 9, 15, 18, 1);
+  var line = '終了日時：2010/10/15 18:01';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 表題
+  info2['title'] = '竜王戦';
+  var line = '表題：竜王戦';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 棋戦
+  info2['event'] = '第23期竜王戦七番勝負第1局';
+  var line = '棋戦：第23期竜王戦七番勝負第1局';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 持ち時間
+  info2['time_limit'] = {allotted: 480};
+  var line = '持ち時間：各8時間';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 消費時間
+  info2['time_consumed'] = {black: 451, white: 457};
+  var line = '消費時間：93▲451△457';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 場所
+  info2['site'] = '長崎・にっしょうかん別邸紅葉亭';
+  var line = '場所：長崎・にっしょうかん別邸紅葉亭';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 先手
+  info2['player_black'] = '渡辺　明';
+  var line = '先手：渡辺　明';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 後手
+  info2['player_white'] = '羽生善治';
+  var line = '後手：羽生善治';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 携帯先手
+  info2['kif']['携帯先手'] = '渡辺';
+  var line = '携帯先手：渡辺';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+
+  // 携帯後手
+  info2['kif']['携帯後手'] = '羽生';
+  var line = '携帯後手：羽生';
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(info, info2, line+' info');
+});
+
+test('parse initial board hirate', 1, function() {
+  var kifu  = kifu_obj.kifu();
+  var board = Kifu.Board().hirate();
+
+  // 平手
+  var line = '手合割：平手　　';
+  console.log(line)
+  ok(Kifu.Kif.parseByLine(line, kifu), line);
+  same(kifu['board'], board, line+' board');
 });
 
 
